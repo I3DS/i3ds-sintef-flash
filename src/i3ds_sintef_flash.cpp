a24b916de97809a9ebd80e1aa203c8207b94a104
Espen Helle <espen.helle@sintef>
2018-08-30 09:47:05 +0200

    Runned style fixing script

--- src/i3ds_sintef_flash.cpp
+++ src/i3ds_sintef_flash.cpp
@@ -28,185 +28,185 @@ volatile bool running;
 
 
 int
-main (int argc, char* argv[])
+main ( int argc, char *argv[] )
 {
-  unsigned int node_id;
-  std::string serialPort;
-  std::string flashCommand;
-  bool triggerFlash = false;
-  bool testMode = false; ///< Ontly test, don't start i3ds flash server
-  std::vector<double> commandParameters;
-  std::vector<double> remoteParameters;
+    unsigned int node_id;
+    std::string serialPort;
+    std::string flashCommand;
+    bool triggerFlash = false;
+    bool testMode = false; ///< Ontly test, don't start i3ds flash server
+    std::vector<double> commandParameters;
+    std::vector<double> remoteParameters;
 
-  po::options_description desc ("Wide angle flash control options");
-  desc.add_options () ("help,h", "Produce this message")
-  ("node,n", po::value<unsigned int>(&node_id)->required(), "Node ID of camera")
+    po::options_description desc ( "Wide angle flash control options" );
+    desc.add_options () ( "help,h", "Produce this message" )
+    ( "node,n", po::value<unsigned int> ( &node_id )->required(), "Node ID of camera" )
 
-  ("device,d", po::value<std::string> ()->default_value (DEFAULT_SERIAL_PORT),
-   "Serial(USB) port the \"Wide angle flash\" is connected to.")
+    ( "device,d", po::value<std::string> ()->default_value ( DEFAULT_SERIAL_PORT ),
+      "Serial(USB) port the \"Wide angle flash\" is connected to." )
 
-  ("trigger-flash,t", "Trigger flash manually.")
+    ( "trigger-flash,t", "Trigger flash manually." )
 
-  ("command,c", po::value<std::string> (),
-   "Manually send command string to interact with the flash.\n"
-   "\tOne may use all command string described in the manual.\n"
-   "\tE.g \"TR1\" for triggering flash.\n"
-   "\tE.g \"RT1,3,200,10.0\" For setting flash parameters.\n"
-   "\tE.g \"ST\" for getting parameter information.\n")
-   (
-      "remote,r",
-      po::value < std::vector<double> > (&commandParameters)->multitoken (),
-      "This is the command available remotely via i3ds."
-      "Actually the used command is: \"RTc,p,d,s,r\"\n"
-      "\tc=1 => Light strobe output.\n\tc=2 => Trigger output signal\n"
-      "\tp= pulse width in milliseconds (0.01 to 3)\n"
-      "\ts= setting in percent(0 to 100)\n"
-      "\tr= re-trigger delay in milliseconds (an optional parameter)\n"
-      "Settings are not keep during a power cycle.\n"
-      "Use format: -c 1 0.1 200 100 12\n")
+    ( "command,c", po::value<std::string> (),
+      "Manually send command string to interact with the flash.\n"
+      "\tOne may use all command string described in the manual.\n"
+      "\tE.g \"TR1\" for triggering flash.\n"
+      "\tE.g \"RT1,3,200,10.0\" For setting flash parameters.\n"
+      "\tE.g \"ST\" for getting parameter information.\n" )
+    (
+        "remote,r",
+        po::value < std::vector<double> > ( &commandParameters )->multitoken (),
+        "This is the command available remotely via i3ds."
+        "Actually the used command is: \"RTc,p,d,s,r\"\n"
+        "\tc=1 => Light strobe output.\n\tc=2 => Trigger output signal\n"
+        "\tp= pulse width in milliseconds (0.01 to 3)\n"
+        "\ts= setting in percent(0 to 100)\n"
+        "\tr= re-trigger delay in milliseconds (an optional parameter)\n"
+        "Settings are not keep during a power cycle.\n"
+        "Use format: -c 1 0.1 200 100 12\n" )
 
-  //("vector_value", po::value<std::vector<double> >(&vecoption)->
-  //     multitoken()->default_value(std::vector<double>{0, 1, 2}), "description");
+    //("vector_value", po::value<std::vector<double> >(&vecoption)->
+    //     multitoken()->default_value(std::vector<double>{0, 1, 2}), "description");
 
-  ("verbose,v", "Print verbose output") ("quite,q", "Quiet output");
+    ( "verbose,v", "Print verbose output" ) ( "quite,q", "Quiet output" );
 
-  po::variables_map vm;
-  po::store (po::parse_command_line (argc, argv, desc), vm);
+    po::variables_map vm;
+    po::store ( po::parse_command_line ( argc, argv, desc ), vm );
 
-  if (vm.count ("help"))
+    if ( vm.count ( "help" ) )
     {
-      BOOST_LOG_TRIVIAL(info) << desc;
-      return -1;
+        BOOST_LOG_TRIVIAL ( info ) << desc;
+        return -1;
     }
 
-  if (vm.count ("device"))
+    if ( vm.count ( "device" ) )
     {
-      serialPort = vm["device"].as<std::string> ();
-      BOOST_LOG_TRIVIAL(info) << "Using serial port: " << serialPort;
+        serialPort = vm["device"].as<std::string> ();
+        BOOST_LOG_TRIVIAL ( info ) << "Using serial port: " << serialPort;
     }
-  if (vm.count ("trigger-flash"))
+    if ( vm.count ( "trigger-flash" ) )
     {
-      BOOST_LOG_TRIVIAL(info)<< "Manually trigger flash";
-      triggerFlash = true;
-      testMode = true;
-    }
-
-  if (vm.count ("command"))
-    {
-      flashCommand = vm["command"].as<std::string> ();
-      BOOST_LOG_TRIVIAL(info) << "Manually send command to flash.";
-      BOOST_LOG_TRIVIAL(info) << "Command:" << flashCommand;
-      testMode = true;
+        BOOST_LOG_TRIVIAL ( info ) << "Manually trigger flash";
+        triggerFlash = true;
+        testMode = true;
     }
 
-  if (vm.count ("remote"))
+    if ( vm.count ( "command" ) )
     {
-      remoteParameters = vm["remote"].as<std::vector<double>> ();
-      BOOST_LOG_TRIVIAL(info) << "Test remote parameters.";
-      BOOST_LOG_TRIVIAL(info) <<  "Commandparameter[1]:" << flashCommand << remoteParameters[0];
-      testMode = true;
+        flashCommand = vm["command"].as<std::string> ();
+        BOOST_LOG_TRIVIAL ( info ) << "Manually send command to flash.";
+        BOOST_LOG_TRIVIAL ( info ) << "Command:" << flashCommand;
+        testMode = true;
     }
 
-  if (vm.count ("quite"))
+    if ( vm.count ( "remote" ) )
     {
-      logging::core::get ()->set_filter (
-	  logging::trivial::severity >= logging::trivial::warning);
-    }
-  else if (!vm.count ("verbose"))
-    {
-      logging::core::get ()->set_filter (
-	  logging::trivial::severity >= logging::trivial::info);
+        remoteParameters = vm["remote"].as<std::vector<double>> ();
+        BOOST_LOG_TRIVIAL ( info ) << "Test remote parameters.";
+        BOOST_LOG_TRIVIAL ( info ) <<  "Commandparameter[1]:" << flashCommand << remoteParameters[0];
+        testMode = true;
     }
 
-  po::notify (vm);
-
-
-
-  BOOST_LOG_TRIVIAL(info) << "Wide angle flash got node ID: " << node_id;
-
-  BOOST_LOG_TRIVIAL(trace) << "---> [OK]";
-
-  i3ds::Context::Ptr context(i3ds::Context::Create());
-  i3ds::Server server(context);
-
-  i3ds::SintefFlash serialCommunicator(node_id, serialPort);
-
-  BOOST_LOG_TRIVIAL(info) << "Attaching server.";
-  serialCommunicator.Attach(server);
-  BOOST_LOG_TRIVIAL(info) << "Starting server.";
-  server.Start();
-  BOOST_LOG_TRIVIAL(info) << "Server started.";
-
-
-
-  if (!flashCommand.empty ())
+    if ( vm.count ( "quite" ) )
     {
-      serialCommunicator.SendString (flashCommand.c_str ());
+        logging::core::get ()->set_filter (
+            logging::trivial::severity >= logging::trivial::warning );
     }
-
-  if (!remoteParameters.empty ())
-    {
-      if (remoteParameters.size () == 4)
-	{
-	   serialCommunicator.setFlashParameters (
-	       remoteParameters[0],
-	       remoteParameters[1],
-	       remoteParameters[2],
-	       remoteParameters[3]
-	   );
-	}
-      if (remoteParameters.size () == 5)
-	{
-	  serialCommunicator.setFlashParameters (
-	      remoteParameters[0],
-	      remoteParameters[1],
-	      remoteParameters[2],
-	      remoteParameters[3],
-	      remoteParameters[4]
-	  );
-	}
-    }
-
-  if (triggerFlash)
-    {
-      serialCommunicator.ManualTrigger ();
-    }
-
-  if (testMode)
-    {
-      serialCommunicator.CloseSerialPort ();
-      return 0;
-    }
-  else
-    {
-      running = true;
-
-      while (running)
+    else
+        if ( !vm.count ( "verbose" ) )
         {
-          sleep(1);
+            logging::core::get ()->set_filter (
+                logging::trivial::severity >= logging::trivial::info );
         }
 
-      server.Stop();
-      serialCommunicator.CloseSerialPort ();
-      return 0;
+    po::notify ( vm );
+
+
+
+    BOOST_LOG_TRIVIAL ( info ) << "Wide angle flash got node ID: " << node_id;
+
+    BOOST_LOG_TRIVIAL ( trace ) << "---> [OK]";
+
+    i3ds::Context::Ptr context ( i3ds::Context::Create() );
+    i3ds::Server server ( context );
+
+    i3ds::SintefFlash serialCommunicator ( node_id, serialPort );
+
+    BOOST_LOG_TRIVIAL ( info ) << "Attaching server.";
+    serialCommunicator.Attach ( server );
+    BOOST_LOG_TRIVIAL ( info ) << "Starting server.";
+    server.Start();
+    BOOST_LOG_TRIVIAL ( info ) << "Server started.";
+
+
+    if ( !flashCommand.empty () )
+    {
+        serialCommunicator.SendString ( flashCommand.c_str () );
+    }
+
+    if ( !remoteParameters.empty () )
+    {
+        if ( remoteParameters.size () == 4 )
+        {
+            serialCommunicator.setFlashParameters (
+                remoteParameters[0],
+                remoteParameters[1],
+                remoteParameters[2],
+                remoteParameters[3]
+            );
+        }
+        if ( remoteParameters.size () == 5 )
+        {
+            serialCommunicator.setFlashParameters (
+                remoteParameters[0],
+                remoteParameters[1],
+                remoteParameters[2],
+                remoteParameters[3],
+                remoteParameters[4]
+            );
+        }
+    }
+
+    if ( triggerFlash )
+    {
+        serialCommunicator.ManualTrigger ();
+    }
+
+    if ( testMode )
+    {
+        serialCommunicator.CloseSerialPort ();
+        return 0;
+    }
+    else
+    {
+        running = true;
+
+        while ( running )
+        {
+            sleep ( 1 );
+        }
+
+        server.Stop();
+        serialCommunicator.CloseSerialPort ();
+        return 0;
     }
 
 
-/*
-// Just Old test code for non-command line testing
- serialCommunicator->sendParameterString ("RT1,3,200,10.0");
+    /*
+    // Just Old test code for non-command line testing
+     serialCommunicator->sendParameterString ("RT1,3,200,10.0");
 
- serialCommunicator->sendManualTrigger ();
- serialCommunicator->sendParameterString ("RT1d3,300.2,1d");
- sleep(1);
- serialCommunicator->sendManualTrigger ();
- sleep(1);
+     serialCommunicator->sendManualTrigger ();
+     serialCommunicator->sendParameterString ("RT1d3,300.2,1d");
+     sleep(1);
+     serialCommunicator->sendManualTrigger ();
+     sleep(1);
 
- serialCommunicator->setFlashParameters (1, 0.2, 0.01, 90);
- sleep(1);
- serialCommunicator->sendManualTrigger ();
- serialCommunicator.CloseSerialPort ();
- return 0;
- */
+     serialCommunicator->setFlashParameters (1, 0.2, 0.01, 90);
+     sleep(1);
+     serialCommunicator->sendManualTrigger ();
+     serialCommunicator.CloseSerialPort ();
+     return 0;
+     */
 
 }
--- src/sintef_flash.cpp
+++ src/sintef_flash.cpp
@@ -22,127 +22,131 @@
 namespace logging = boost::log;
 
 
-i3ds::SintefFlash::SintefFlash (NodeID nodeID, std::string port): Flash(nodeID)
+i3ds::SintefFlash::SintefFlash ( NodeID nodeID, std::string port ) : Flash ( nodeID )
 {
-  BOOST_LOG_TRIVIAL(info) << "SintefFlash constructor";
-  BOOST_LOG_TRIVIAL(info) << "Connecting to serial port: " << port;
+    BOOST_LOG_TRIVIAL ( info ) << "SintefFlash constructor";
+    BOOST_LOG_TRIVIAL ( info ) << "Connecting to serial port: " << port;
 
-  memset (fds, 0, sizeof(pollfd));
-  fds[0].fd = OpenSerialPort(port.c_str());
+    memset ( fds, 0, sizeof ( pollfd ) );
+    fds[0].fd = OpenSerialPort ( port.c_str() );
 
 }
 
 i3ds::SintefFlash::~SintefFlash()
 {
-  BOOST_LOG_TRIVIAL(info) << "i3ds::SintefFlash Destructor";
-  CloseSerialPort ();
+    BOOST_LOG_TRIVIAL ( info ) << "i3ds::SintefFlash Destructor";
+    CloseSerialPort ();
 }
 
 void
 i3ds::SintefFlash::CloseSerialPort()
 {
-  BOOST_LOG_TRIVIAL(info) << "Closing Serial port";
-  close(fds[0].fd);
+    BOOST_LOG_TRIVIAL ( info ) << "Closing Serial port";
+    close ( fds[0].fd );
 }
 
 void
 i3ds::SintefFlash::ManualTrigger ()
 {
-  BOOST_LOG_TRIVIAL(info) << "Sending manual trigger";
-  SendString("TR1");
+    BOOST_LOG_TRIVIAL ( info ) << "Sending manual trigger";
+    SendString ( "TR1" );
 }
 
 /// This is a parameter string as described in the manual.
 void
-i3ds::SintefFlash::SendString (const char* parameter)
+i3ds::SintefFlash::SendString ( const char *parameter )
 {
-  const int lengthOfBuff = 100;
-  char buff[lengthOfBuff];
-  memset (buff, 0, sizeof(buff));
-  BOOST_LOG_TRIVIAL(info) << "Sending parameter string: " << parameter;
+    const int lengthOfBuff = 100;
+    char buff[lengthOfBuff];
+    memset ( buff, 0, sizeof ( buff ) );
+    BOOST_LOG_TRIVIAL ( info ) << "Sending parameter string: " << parameter;
 
-  char command[lengthOfBuff];
-  memset (command, 0,lengthOfBuff-1);
-  strncpy (command, parameter, lengthOfBuff-1);
-  strcat (command, "\n");
+    char command[lengthOfBuff];
+    memset ( command, 0, lengthOfBuff - 1 );
+    strncpy ( command, parameter, lengthOfBuff - 1 );
+    strcat ( command, "\n" );
 
-  ssize_t retval = write (fds[0].fd, command, strlen (command));
-  BOOST_LOG_TRIVIAL(info) << "strlen(command) " << strlen (command) << ",  return value(write): " << retval;
-  tcdrain (fds[0].fd);
+    ssize_t retval = write ( fds[0].fd, command, strlen ( command ) );
+    BOOST_LOG_TRIVIAL ( info ) << "strlen(command) " << strlen ( command ) << ",  return value(write): " << retval;
+    tcdrain ( fds[0].fd );
 
-  // Waiting for response
+    // Waiting for response
 
-  fds[0].events = POLLIN;
-  fds[0].revents = 0;
-  int n;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+    int n;
 
-  n = poll (fds, 1, 2000);
+    n = poll ( fds, 1, 2000 );
 
-  if (n > 0)
+    if ( n > 0 )
     {
-      if (fds[0].revents & POLLIN)
-	{ //got data, and look up which fd has data, but we just have one waiting for events
-	  ssize_t length;
-	  length = read (fds[0].fd, buff, lengthOfBuff);
-	  if (length == -1)
-	    {
-	      // REMARK: Got "Resource temporary unavailable." sometimes.
-	      // But, I think it disapaired when I removed a terminal listening to the same serial port.
-	      BOOST_LOG_TRIVIAL(info) <<"Error read event: " << strerror (errno);
-	    }
-	  buff[length] = 0;
-	 // BOOST_LOG_TRIVIAL(info) << "From poll \"" << buff<<"\"";
+        if ( fds[0].revents & POLLIN )
+        {
+            //got data, and look up which fd has data, but we just have one waiting for events
+            ssize_t length;
+            length = read ( fds[0].fd, buff, lengthOfBuff );
+            if ( length == -1 )
+            {
+                // REMARK: Got "Resource temporary unavailable." sometimes.
+                // But, I think it disapaired when I removed a terminal listening to the same serial port.
+                BOOST_LOG_TRIVIAL ( info ) << "Error read event: " << strerror ( errno );
+            }
+            buff[length] = 0;
+            // BOOST_LOG_TRIVIAL(info) << "From poll \"" << buff<<"\"";
 
-	  // This part replaces '\n' with '<NL>' for debug printout purposes
-	  char buff2[2*lengthOfBuff];
-	  int j = 0;
-	  for(int i=0; (buff[i] != '\0') && (i < lengthOfBuff); i++)
-	    {
-	      if(buff[i] == '\n')
-		{
-		  strncpy(&buff2[j], "<NL>", 4);
-		  j = j + 3;
-		}
-	      else
-		if(buff[i] == '\r')
-		  {
-		    strncpy(&buff2[j], "<CR>", 4);
-		    j = j + 3;
-		  }
-		else
-		  {
-		    buff2[j] = buff[i];
-		  }
-	      buff2[j+1] = '\0';
-	      j++;
-	    }
+            // This part replaces '\n' with '<NL>' for debug printout purposes
+            char buff2[2 * lengthOfBuff];
+            int j = 0;
+            for ( int i = 0; ( buff[i] != '\0' ) && ( i < lengthOfBuff ); i++ )
+            {
+                if ( buff[i] == '\n' )
+                {
+                    strncpy ( &buff2[j], "<NL>", 4 );
+                    j = j + 3;
+                }
+                else
+                    if ( buff[i] == '\r' )
+                    {
 
-	  BOOST_LOG_TRIVIAL(info) << "From poll:(NL & CR replaced) \"" << buff2<<"\"";
-	  BOOST_LOG_TRIVIAL(info) << "Received Length:" << length;
+                        strncpy ( &buff2[j], "<CR>", 4 );
+                        j = j + 3;
+                    }
+                    else
+                    {
+                        buff2[j] = buff[i];
+                    }
+                buff2[j + 1] = '\0';
+                j++;
+            }
+
+            BOOST_LOG_TRIVIAL ( info ) << "From poll:(NL & CR replaced) \"" << buff2 << "\"";
+            BOOST_LOG_TRIVIAL ( info ) << "Received Length:" << length;
 
 
-	  BOOST_LOG_TRIVIAL(info) << "Ok response.\n";
-	  if(strstr(buff, "Err 5") != NULL)
-	     {
-	       BOOST_LOG_TRIVIAL(info) << "Outside limits for combination strength & duration. Strength kept, duration adjusted";
-	       throw i3ds::CommandError(error_value, "Outside limits for combination strength & duration. Strength kept, duration adjusted");
-	     }
+            BOOST_LOG_TRIVIAL ( info ) << "Ok response.\n";
+            if ( strstr ( buff, "Err 5" ) != NULL )
+            {
+                BOOST_LOG_TRIVIAL ( info ) << "Outside limits for combination strength & duration. Strength kept, duration adjusted";
+                throw i3ds::CommandError ( error_value, "Outside limits for combination strength & duration. Strength kept, duration adjusted" );
+            }
 
-	  if(strstr(buff, "Err ") != NULL)
-	   {
-	     BOOST_LOG_TRIVIAL(info) << "Error in data string sent to serial port.";
-	     throw i3ds::CommandError(error_value, "Error in data string sent to serial port: " + std::string(buff));
-	   }
-	}
-      else
-	{
-	  BOOST_LOG_TRIVIAL(info) <<"Other event type? Needs to be handled?";
-	}
+            if ( strstr ( buf
+                          f, "Err " ) != NULL )
+            {
+                BOOST_LOG_TRIVIAL ( info ) << "Error in data string sent to serial port.";
+                throw i3ds::CommandError ( error_value, "Error in data string sent to serial port: " + std::string ( buff ) );
+            }
+        }
+        else
+        {
+            BOOST_LOG_TRIVIAL ( info ) << "Other event type? Needs to be handled?";
+        }
     }
-  else
+    else
     {
-      BOOST_LOG_TRIVIAL(info) << "No data received from serial port within 2 seconds.";
+        BOOST_LOG_TRIVIAL ( info ) << "No data received from serial port within 2 seconds.";
     }
+
 
 }
 
@@ -151,68 +155,68 @@ i3ds::SintefFlash::SendString (const char* parameter)
 // Limits are described in manual#include <boost/program_options.hpp>
 void
 i3ds::SintefFlash::setFlashParameters ( int c, 	///< 1 – light strobe output; 2 – Trigger output signal.
-					float p, ///< Pulsewidth in milliseconds (0.01 to 3)
-					float d, ///< Delay from trigger to pulse in milliseconds (0.01 to 999)
-					float s, ///< Strength setting i percent (0 to 100)
-					float r 	///< Re-trigger delay in milliseconds (optional parameter)
-				      )
+                                        float p, ///< Pulsewidth in milliseconds (0.01 to 3)
+                                        float d, ///< Delay from trigger to pulse in milliseconds (0.01 to 999)
+                                        float s, ///< Strength setting i percent (0 to 100)
+                                        float r 	///< Re-trigger delay in milliseconds (optional parameter)
+                                      )
 {
 
-  char buffer[100];
+    char buffer[100];
 
-  BOOST_LOG_TRIVIAL(info) <<"Sending configuration parameters: " << "c:" << c << " "
-      "p:" << p << " "
-      "d:" << d << " "
-      "s:" << s << " ";
+    BOOST_LOG_TRIVIAL ( info ) << "Sending configuration parameters: " << "c:" << c << " "
+                               "p:" << p << " "
+                               "d:" << d << " "
+                               "s:" << s << " ";
 
-  if (r != -1.0)
+    if ( r != -1.0 )
     {
-      BOOST_LOG_TRIVIAL(info) << "r: " << r;
-      sprintf (buffer, "RT%d,%g,%g,%g,%g", c, p, d, s, r);
+        BOOST_LOG_TRIVIAL ( info ) << "r: " << r;
+        sprintf ( buffer, "RT%d,%g,%g,%g,%g", c, p, d, s, r );
     }
-  else
+    else
     {
-      sprintf (buffer, "RT%d,%g,%g,%g", c, p, d, s);
+        sprintf ( buffer, "RT%d,%g,%g,%g", c, p, d, s );
     }
 
-  SendString (buffer);
+    SendString ( buffer );
 
 }
 
 void
-i3ds::SintefFlash::SetSerialCommunicationParameters (int fd)
+i3ds::SintefFlash::SetSerialCommunicationParameters ( int fd )
 {
-  BOOST_LOG_TRIVIAL(info) << "setConfiguration";
-  struct termios Opt;
-  tcgetattr (fd, &Opt);
-  cfsetispeed (&Opt, B115200);
-  cfsetospeed (&Opt, B115200);
-  tcsetattr (fd, TCSANOW, &Opt);
+    BOOST_LOG_TRIVIAL ( info ) << "setConfiguration";
+    struct termios Opt;
+    tcgetattr ( fd, &Opt );
+    cfsetispeed ( &Opt, B115200 );
+    cfsetospeed ( &Opt, B115200 );
+    tcsetattr ( fd, TCSANOW, &Opt );
 
-  struct termios options;
-  tcgetattr (fd, &options);
-  options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-  options.c_oflag &= ~OPOST;
-  int retcode = tcsetattr (fd, TCSANOW, &options);
-  BOOST_LOG_TRIVIAL(info) << "tcsetattr return code: " << retcode;
-  return;
+    struct termios options;
+    tcgetattr ( fd, &options );
+    options.c_lflag &= ~ ( ICANON | ECHO | ECHOE | ISIG );
+    options.c_oflag &= ~OPOST;
+    int retcode = tcsetattr ( fd, TCSANOW, &options );
+    BOOST_LOG_TRIVIAL ( info ) << "tcsetattr return code: " << retcode;
+    return;
 }
 
 int
-i3ds::SintefFlash::OpenSerialPort(const char *device)
+i3ds::SintefFlash::OpenSerialPort ( const char *device )
 {
-  int fd = open (device, O_RDWR | O_NOCTTY | O_NDELAY);  //| O_NOCTTY | O_NDELAY
-  if (-1 == fd)
+    int fd = open ( device, O_RDWR | O_NOCTTY | O_NDELAY ); //| O_NOCTTY | O_NDELAY
+    if ( -1 == fd )
     {
-      BOOST_LOG_TRIVIAL(info) << "Can't Open Serial Port";
-      BOOST_LOG_TRIVIAL(info) << "Exiting.";
-      exit(1);
-      return -1;
+        BOOST_LOG_TRIVIAL ( info ) << "Can't Open Serial Port";
+        BOOST_LOG_TRIVIAL ( info ) << "Exiting.";
+        exit ( 1 );
+        return -1;
     }
-  else
+    else
     {
-      SetSerialCommunicationParameters (fd);
-      return fd;
+        SetSerialCommunicationParameters ( fd );
+        return fd;
     }
 
 }
@@ -222,55 +226,55 @@ i3ds::SintefFlash::OpenSerialPort(const char *device)
 /// REMARK: We are not handling spontainous overhead messages
 /// REMARK: Duty cycle limitations has to be handled by camera which knows the parameters.
 void
-i3ds::SintefFlash::handle_flash(FlashService::Data& command)
+i3ds::SintefFlash::handle_flash ( FlashService::Data &command )
 {
-  BOOST_LOG_TRIVIAL(info) << "handle_flash";
-  int strength = command.request.strength;
-  int duration_us = command.request.duration;
+    BOOST_LOG_TRIVIAL ( info ) << "handle_flash";
+    int strength = command.request.strength;
+    int duration_us = command.request.duration;
 
-  BOOST_LOG_TRIVIAL(info) << "Strength: " << strength;
-  BOOST_LOG_TRIVIAL(info) << "Duration in us: " << duration_us;
+    BOOST_LOG_TRIVIAL ( info ) << "Strength: " << strength;
+    BOOST_LOG_TRIVIAL ( info ) << "Duration in us: " << duration_us;
 
-  if ((strength < 0) || (strength > 100) )
+    if ( ( strength < 0 ) || ( strength > 100 ) )
     {
-      BOOST_LOG_TRIVIAL(info) << "Strength must be within 0-100%.";
-      throw i3ds::CommandError(error_value, "Flash Strength must be within 0-100%.");
+        BOOST_LOG_TRIVIAL ( info ) << "Strength must be within 0-100%.";
+        throw i3ds::CommandError ( error_value, "Flash Strength must be within 0-100%." );
     }
 
-  if ((duration_us < 10) || (duration_us > 3000000) )
+    if ( ( duration_us < 10 ) || ( duration_us > 3000000 ) )
     {
-      BOOST_LOG_TRIVIAL(info) << "Duration must be within 0.01-3000ms.";
-      throw i3ds::CommandError(error_value, "Flash duration must be within 0-3000ms.");
+        BOOST_LOG_TRIVIAL ( info ) << "Duration must be within 0.01-3000ms.";
+        throw i3ds::CommandError ( error_value, "Flash duration must be within 0-3000ms." );
     }
-  float flash_duration_ms = duration_us / 1000.;
+    float flash_duration_ms = duration_us / 1000.;
 
-  /// Remark: Err 5 is a out of range warning for one parameter.
-        /// It is fixed to valid value.
-        /// But, it looks as there is a limit with a relationship between duration and flash strength
-        /// Explained here:
-	 //   http://www.gardasoft.com/downloads/?f=112
-        //  Page 13(Read it)
-        ///
-        //Output brightness		850nm variant    | 			|	White variant
-        //			Allowed pulsewidth |Allowed duty cycle 	| Allowed pulse width   |   Allowed duty cycle
-        // 0% to  20% 		3ms 			6% 			3ms 			3%
-        //21% to  30% 		3ms 			6%	 		2ms 			3%
-        //31% to  50% 		3ms 			3% 			2ms 			2%
-        //51% to 100% 		2ms 			3% 			1ms 			1%
+    /// Remark: Err 5 is a out of range warning for one parameter.
+    /// It is fixed to valid value.
+    /// But, it looks as there is a limit with a relationship between duration and flash strength
+    /// Explained here:
+    //   http://www.gardasoft.com/downloads/?f=112
+    //  Page 13(Read it)
+    ///
+    //Output brightness		850nm variant    | 			|	White variant
+    //			Allowed pulsewidth |Allowed duty cycle 	| Allowed pulse width   |   Allowed duty cycle
+    // 0% to  20% 		3ms 			6% 			3ms 			3%
+    //21% to  30% 		3ms 			6%	 		2ms 			3%
+    //31% to  50% 		3ms 			3% 			2ms 			2%
+    //51% to 100% 		2ms 			3% 			1ms 			1%
 
 
 
-	    // After some, expremental testing.(It is not described in manual.)
-	    // It looks like there is problem with the strength-duration ratio.
-	    // It vil give the Err 5 warning. Set the actual strength and decrease duration of the pulse to fit the constraints
+    // After some, expremental testing.(It is not described in manual.)
+    // It looks like there is problem with the strength-duration ratio.
+    // It vil give the Err 5 warning. Set the actual strength and decrease duration of the pulse to fit the constraints
 
 
 
     setFlashParameters (
-			  1, 			// Configure strobe output
-			  flash_duration_ms,		// Pulse width ms
-			  0.01, 		// Delay from trigger to pulse in ms(0.01 to 999)
-			  strength		/// Settings in percent
+        1, 			// Configure strobe output
+        flash_duration_ms,		// Pulse width ms
+        0.01, 		// Delay from trigger to pulse in ms(0.01 to 999)
+        strength		/// Settings in percent
     ); 			// 5th parameter retrigger delay in ms(optional not used)
 
 
